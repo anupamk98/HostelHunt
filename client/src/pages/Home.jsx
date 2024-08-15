62e5abbb30447cb2db23ce2fad5c9b802cccfe6e
anupamk98 <142046010+anupamk98@users.noreply.github.com>
2024-08-15 23:25:19 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -105,7 +105,7 @@ export default function Home() {
 
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
         {offerListings && offerListings.length > 0 && (
-          <div className=''>
+          <div className='mb-10'>
             <div className='my-3'>
               <h2 className='text-2xl font-semibold text-slate-600'>Recent offers for Third Years</h2>
               <Link className='text-sm text-blue-800 hover:underline' to={'/search?offer=true'}>Show more offers</Link>
